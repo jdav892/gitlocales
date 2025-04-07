3076d89b99d4eb7797bc38128b8dd8e3684ccd6a
jdav892 <jadavila9@gmail.com>
2025-04-07 00:59:14 -0400

    feat: creates recursive scan functions

--- main.go
+++ main.go
@@ -20,9 +20,44 @@ func scan(folder string) {
 //Returns the base folder of the repo, the .git folder parent.
 //Recursively searches in the subfolders by passing an existing 'folders' slice.
 func scanGitFolders(folder []string, folder string) []string {
-  
+  // trim the last '/'
+  folder = strings.TrimSuffix(folder, "/")
+
+  f, err := os.Open(folder)
+  if err != nil {
+    log.Fatal(err)
+  }
+  files, err := f.Readdir(-1)
+  f.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  var path string
+
+  for _, file := range files {
+    if file.IsDir() {
+      path = folder + "/" + file.Name()
+      if file.Name() == ".git" {
+        path = strings.TrimSuffix(path, "/.git")
+        fmt.Println(path)
+        folders = append(folders, path)
+        continue
+      }
+      if file.Name() == "vendor" || file.Name() == "node_modules" {
+        continue
+      }
+      folders = scanGitFolders(folders, path)
+    }
+  }
+  return folders
 }
 
+//recursiveScanFolder starts the recursive search of git repositories
+//living in the 'folder' subtree
+func recursiveScanFolder(folder string) []string {
+  return scanGitFolders(make([]string, 0), folder)
+}
 //stats generates a nice graph of your Git contributions
 func stats(email string) {
   print("stats")
