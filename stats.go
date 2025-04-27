31e8d26d9f3e9591a85e18e80cd055c9c70d37ae
jdav892 <jadavila9@gmail.com>
2025-04-27 14:45:33 -0400

    feat: creates a function to fill in commit graph

--- stats.go
+++ stats.go
@@ -28,3 +28,44 @@ func processRepositories(email string) map[int]int {
   return commits
 }
 
+//fillCommits given a repository found in `path`, gets the commits and 
+//puts them in the `commits` map, returning it when completed
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+  //instantiate a git repo object from path
+  repo, err := git.PlainOpen(path)
+  if err != nil {
+    panic(err)
+  }
+  //get the HEAD reference
+  ref, err := repo.Head()
+  if err != nil {
+    panic(err)
+  }
+  //get the commits history starting from HEAD
+  iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+  if err != nil {
+    panic(err)
+  }
+  //iterate the commits
+  offset := calcOffset()
+  err = iterator.ForEach(func(c *object.Commit) error {
+    daysAgo := countDaysSinceDate(c.Auther.When) + offset
+    
+    if c.Author.Email != email {
+      return nil
+    }
+
+    if daysAgo != outOfRange {
+      commits[daysAgo]++
+    }
+
+    return nil
+
+  })
+  if err != nil {
+    panic(err)
+  }
+
+  return commits
+}
+
