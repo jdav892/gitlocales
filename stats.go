3944be96c9697d0e90849597393052b6bd4e6a1e
jdav892 <jadavila9@gmail.com>
2025-04-30 02:36:24 -0400

    feat: creates a function to print the months from the beggining of the history that's being analyzed

--- stats.go
+++ stats.go
@@ -186,3 +186,24 @@ func printCells(cols map[int]column) {
     fmt.Printf("\n")
   }
 }
+
+//printMonths prints the month names in the first line, determining when the month
+//changed between switching weeks 
+func printMonths() {
+  week := getBeginningOfDay(time.Now()).Add(-(daysInLastSixMonths * time.Hour * 24))
+  month := week.Month()
+  fmt.Printf("         ")
+  for {
+    if week.Month() != month {
+      fmt.Printf("%s ", week.Month().String()[:3])
+      month = week.Month()
+    } else {
+      fmt.Printf("    ")
+    }
+    week = week.Add(7 * time.Hour * 24)
+    if week.After(time.Now()) {
+      break
+    }
+  }
+  fmt.Printf("\n")
+}
