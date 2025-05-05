d1961995b133984f25941954228ceb334511e33c
jdav892 <jadavila9@gmail.com>
2025-05-05 04:18:45 -0400

    feat: updated go git to v5 and fix plumbing object problem, also fixes os file creation in scan.go

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
-package main 
+package main
 
 import (
-  "flag"
+	"flag"
 )
 
 func main() {
-  var folder string
-  var email string
-  flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
-  flag.StringVar(&email, "email", "your@email.com", "the email to scan")
-  flag.Parse()
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
 
-  if folder != "" {
-    scan(folder)
-    return
-  }
+	if folder != "" {
+		scan(folder)
+		return
+	}
 
-  stats(email)
+	stats(email)
 
 }
